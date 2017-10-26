e28b79ae1788369364d7255ce92787ff63825516
Feng Xue <xue.ziheng@gmail.com>
2017-10-26 17:15:57 +1100

    Browserstack integration

--- src/utility/Browser.java
+++ src/utility/Browser.java
@@ -1,12 +1,14 @@
 package utility;
 
 /**
- * Created by ivanxue on 3/08/2016.
+ * Created by ivanxue on 11/08/2017.
  */
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -17,35 +19,43 @@ import java.net.URL;
 public class Browser {
 
     public static WebDriver driver;
-    public static final String KEY = "868c9ff90ed032ecfa27bd48ed89844a";
-    public static final String SECRET = "af37e9afa932c9cb40133a7d28bc4ab0";
-    public static final String URL = "http://" + KEY + ":" + SECRET + "@hub.testingbot.com/wd/hub";
+    
+    // remote server configuration in browserstack
+    public static final String USERNAME = "ivanxue1";
+    public static final String AUTOMATE_KEY = "YYQosi7Fa5UYvYxy6rCV";
+    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 
     public static DesiredCapabilities caps = new DesiredCapabilities();
 
-
+    // please download and change the driver name based on your system
     public static WebDriver getBrowser(String browserType) throws MalformedURLException {
         switch (browserType) {
             case "firefox":
-                System.setProperty("webdriver.gecko.driver", Constant.driverPath + "geckodriver");
-                return	driver = new FirefoxDriver();
+            System.setProperty("webdriver.gecko.driver", Constant.driverPath + "geckodriver");
+            return  driver = new FirefoxDriver();
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", Constant.driverPath + "chromedriver");
-                return	driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("no-sandbox");
+            System.setProperty("webdriver.chrome.driver", Constant.driverPath + "chromedriver");
+            return  driver = new ChromeDriver();
+            case "htmlunit":
+            return  driver = new HtmlUnitDriver();
             case "safari":
-                return	driver = new SafariDriver();
-            case "IE":
-                System.setProperty("webdriver.ie.driver", Constant.driverPath + "IEDriverServer.exe");
-                return	driver = new InternetExplorerDriver();
+            return  driver = new SafariDriver();
+            case "edge":
+            System.setProperty("webdriver.edge.driver", Constant.driverPath + "MicrosoftWebDriver.exe");
+            return  driver = new EdgeDriver();
             case "Remote":
-                caps.setCapability("browserName", "IE");
-                caps.setCapability("version", "11");
-                caps.setCapability("platform", "WIN10");
+            caps.setCapability("browser", "IE");
+            caps.setCapability("browser_version", "7.0");
+            caps.setCapability("os", "Windows");
+            caps.setCapability("os_version", "XP");
+            caps.setCapability("browserstack.debug", "true");
 
-                return driver = new RemoteWebDriver(new URL(URL), caps);
+            return driver = new RemoteWebDriver(new URL(URL), caps);
             default:
-                System.out.println("browser : " + browserType + " is invalid, Launching Firefox as browser of choice..");
-                return driver = new FirefoxDriver();
+            System.out.println("browser: " + browserType + " is invalid, Launching Firefox as browser of choice..");
+            return driver = new FirefoxDriver();
         }
     }
 }
